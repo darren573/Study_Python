e91eb33b218702a40be4f1edc21e271e1c868b91
darren573 <1415754221@qq.com>
2017-10-14 11:33:05 +0800

    简学Python

---
+++ Test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding UTF-8 -*-
+print("Hello World")
+days = ['Monday', 'Tuesday', '星期三', '星期四', '星期五', '星期六', 'sunday']
+print(days)
+# 第一个注释
+'total = item_one + \
+         item_two + \
+         item_three'
+# 第二个注释
+word = 'word'
+sentence = "这是一个句子"
+paragraph = """这是一个段落"""
+print(word);
+print(sentence);
+print(paragraph)
+
+x = "a"
+y = "b"
+#输出换行
+print (x)
+print (y)
+#不换行输出
+print(x),print(y)
+#不换行输出
+print(x),
+print(y),
---
+++ TestTwo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding UTF-8 -*-
+a=5;b=7
+if a>b:
+    print(a)
+else:
+    print(b)
